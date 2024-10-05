ff1e1f7df0d9faf6b06a1e537297db3b2b8105bc
Theophelus <aneletom10@gmail.com>
2024-10-05 15:28:54 +0200

    adding BaseCore.java methods to handle common functionality
    - Adding a initBrowser method, that can be easily extend to support different browsers
    - implementing webDriver cleanup with tearDown method

--- src/main/java/org/anele/base/BaseCore.java
+++ src/main/java/org/anele/base/BaseCore.java
@@ -1,5 +1,45 @@
 package org.anele.base;
 
-public class BaseCore {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ThreadGuard;
 
+import java.time.Duration;
+
+public class BaseCore {
+    //define a Thread safe WebDrivers, to be able to use single thread of webDrivers a time
+    protected static ThreadLocal<WebDriver> WEB_DRIVERS = new ThreadLocal<>();
+
+    //get web drivers current instance
+    public WebDriver getDrivers() {
+        return WEB_DRIVERS.get();
+    }
+
+    public void initBrowser(String browser) {
+
+        if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            WEB_DRIVERS = new ThreadLocal<>();
+        } else {
+            throw new RuntimeException("Provided browser not supported: " + browser);
+        }
+
+        //set a thread guard to ensure each thread is used.
+        WEB_DRIVERS.set(ThreadGuard.protect(new EdgeDriver()));
+
+        //configure drivers
+        getDrivers().manage().window().maximize();
+        getDrivers().manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    //clean up webDriver resources
+    public void tearDown() {
+        if (getDrivers() != null) {
+            getDrivers().quit();
+            WEB_DRIVERS.remove();//remove driver thread instance after each run
+        }
+    }
 }
