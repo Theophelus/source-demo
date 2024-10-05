832cfdd74b358156aba6e77923c96743d4b92337
Theophelus <aneletom10@gmail.com>
2024-10-05 19:34:49 +0200

    Adding locators for Login page and a constructo

--- src/main/java/org/anele/pages/LoginPage.java
+++ src/main/java/org/anele/pages/LoginPage.java
@@ -1,4 +1,25 @@
 package org.anele.pages;
 
+import org.anele.base.BaseCore;
+import org.anele.utils.ConfigManager;
+import org.openqa.selenium.By;
+
 public class LoginPage {
+
+    //define base_core object, to reuse reusable methods
+    protected BaseCore baseCore;
+    protected ConfigManager configManager;
+    //define locator
+    protected By username = By.id("user-name");
+    protected By password = By.id("password");
+    protected By button = By.id("login-button");
+//    protected By error_message = By.className("error-message-container error");
+//    protected By error_button = By.className("error-button");
+
+    //add a construct, then initialize Base Core object.
+    public LoginPage() {
+        baseCore = new BaseCore();
+        configManager = new ConfigManager();
+    }
+
 }
