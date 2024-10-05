0c68d4dc2c6725bcdcbcbbe0c508477da379d96b
Theophelus <aneletom10@gmail.com>
2024-10-05 15:50:25 +0200

    Adding a ConfigManager.java class to handle config file, with a method to load properties file

---
+++ src/main/java/org/anele/utils/ConfigManager.java
@@ -0,0 +1,29 @@
+package org.anele.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigManager {
+    protected static Properties properties;
+
+    public ConfigManager() {
+        properties = new Properties();
+        loadProperties();
+    }
+
+    //load properties
+    public void loadProperties() {
+        //define a path for config.properties file
+        String path = "src/main/resources/config.properties";
+        try (FileInputStream stream = new FileInputStream(path)) {
+            if (stream == null)
+                throw new IOException("unable to locate provided file path: " + path);
+            //load file properties
+            properties.load(stream);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
