05d81f7c7a168e5c7b2049430ff89223d86204d0
Theophelus <aneletom10@gmail.com>
2024-10-05 19:08:09 +0200

    Adding a method to populate text boxes for required values

--- src/main/java/org/anele/base/BaseCore.java
+++ src/main/java/org/anele/base/BaseCore.java
@@ -2,10 +2,13 @@ package org.anele.base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ThreadGuard;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
@@ -23,11 +26,9 @@ public class BaseCore {
 
         if (browser.equalsIgnoreCase("edge")) {
             WebDriverManager.edgedriver().setup();
-            WEB_DRIVERS = new ThreadLocal<>();
         } else {
             throw new RuntimeException("Provided browser not supported: " + browser);
         }
-
         //set a thread guard to ensure each thread is used.
         WEB_DRIVERS.set(ThreadGuard.protect(new EdgeDriver()));
 
@@ -53,5 +54,23 @@ public class BaseCore {
     //findElements multiple elements
     public List<WebElement> finds(By locator) {
         return getDrivers().findElements(locator);
+    }
+
+    //method to typeValues into text_boxes
+    public void typeRequiredValue(By locator, String value) {
+        WebElement element = find(locator);
+        String attribute = element.getAttribute("value");
+
+        try {
+            //check if element is not empty
+            if (!attribute.isEmpty()) {
+                element.sendKeys(Keys.CONTROL, "a");
+                element.sendKeys(Keys.BACK_SPACE);
+            }
+            // if empty type provided value
+            element.sendKeys(value);
+        } catch (Exception e) {
+            System.out.println("Provided element not found: " + element.getText() + " " + e.getMessage());
+        }
     }
 }
