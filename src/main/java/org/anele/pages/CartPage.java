63a9a468e8c62228fa27e3880f7fc0b02033fc76
Theophelus <aneletom10@gmail.com>
2024-10-06 19:59:01 +0200

    Adding a cart page functionality

--- src/main/java/org/anele/pages/CartPage.java
+++ src/main/java/org/anele/pages/CartPage.java
@@ -1,4 +1,31 @@
 package org.anele.pages;
 
+import org.anele.base.BaseCore;
+import org.openqa.selenium.By;
+
 public class CartPage {
+
+    protected BaseCore baseCore;
+
+    protected By cart_header = By.xpath("//div[@class=\"header_secondary_container\"]/span");
+    protected By checkout_button = By.id("checkout");
+
+    public CartPage() {
+        this.baseCore = new BaseCore();
+    }
+
+    //verify cart header text
+    public String verify_your_cart_header(String value) {
+
+        String cart_header_text = baseCore.find(cart_header).getText();
+        if (cart_header_text.equalsIgnoreCase(value))
+            return cart_header_text;
+        else
+            throw new AssertionError("Expected header text: '" + value + "'" + " " + "but found: '" + cart_header_text + "' ");
+    }
+
+    //click check out button
+    public void click_checkout_button() {
+        baseCore.click(checkout_button);
+    }
 }
