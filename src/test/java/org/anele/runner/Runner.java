96ac89aa422e345caabb36868301bd0971c77935
Theophelus <aneletom10@gmail.com>
2024-10-06 21:06:05 +0200

    updating test Runner.java class

--- src/test/java/org/anele/runner/Runner.java
+++ src/test/java/org/anele/runner/Runner.java
@@ -5,7 +5,10 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"org.anele.stepDef", "org.anele.hooks"}
+        glue = {"org.anele.stepDef", "org.anele.hooks"},
+        tags = "not @wip",
+        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
+        monochrome = true
 )
 public class Runner extends AbstractTestNGCucumberTests {
 
