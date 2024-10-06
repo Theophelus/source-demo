a0791633964679f7dc994fbe3f680f8e77ba0287
Theophelus <aneletom10@gmail.com>
2024-10-06 18:36:55 +0200

    Adding CartPage.java and CheckoutPage.java classes

---
+++ src/main/java/org/anele/pages/CartPage.java
@@ -0,0 +1,4 @@
+package org.anele.pages;
+
+public class CartPage {
+}
---
+++ src/main/java/org/anele/pages/CheckoutPage.java
@@ -0,0 +1,8 @@
+package org.anele.pages;
+
+import org.openqa.selenium.By;
+
+public class CheckoutPage {
+    protected By cart_header = By.xpath("//div[@class=\"header_secondary_container\"]/span");
+    protected By checkout_button = By.id("checkout");
+}
