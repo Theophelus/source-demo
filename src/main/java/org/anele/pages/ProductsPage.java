77202b85438dd620c0c9d716c1a1ede1815a4da4
Theophelus <aneletom10@gmail.com>
2024-10-06 13:01:48 +0200

    Adding a ProductsPage.java class with locators needed, and a method to add items in the cart dynamically

---
+++ src/main/java/org/anele/pages/ProductsPage.java
@@ -0,0 +1,33 @@
+package org.anele.pages;
+
+import org.anele.base.BaseCore;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class ProductsPage {
+
+    protected BaseCore baseCore;
+    protected By product_name = By.xpath("//div[@class=\"inventory_item_name \"]");
+    protected By cart_index = By.xpath("//span[@class=\"shopping_cart_badge\"]");
+    protected By add_to_cart_icon = By.xpath("//div[@id=\"shopping_cart_container\"]");
+
+    public ProductsPage() {
+        this.baseCore = new BaseCore();
+    }
+
+    public void add_items_to_cart(String item) {
+        List<WebElement> elementList = baseCore.finds(product_name);
+        boolean isFound = false;
+
+        //if product list and item list are empty, throw an exception
+        if (elementList.isEmpty()) {
+            throw new NoSuchElementException("Provided lists are empty");
+        }
+
+
+    }
+
+}
