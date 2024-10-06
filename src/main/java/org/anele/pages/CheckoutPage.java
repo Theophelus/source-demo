999c3deca02faf5f5d47e1216a7b8bb0a3f3b9dd
Theophelus <aneletom10@gmail.com>
2024-10-06 20:01:50 +0200

    CheckoutPage.java class with its locators and fill form checkout with Faker library

--- src/main/java/org/anele/pages/CheckoutPage.java
+++ src/main/java/org/anele/pages/CheckoutPage.java
@@ -1,8 +1,34 @@
 package org.anele.pages;
 
+import com.github.javafaker.Faker;
+import org.anele.base.BaseCore;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.Locale;
 
 public class CheckoutPage {
-    protected By cart_header = By.xpath("//div[@class=\"header_secondary_container\"]/span");
-    protected By checkout_button = By.id("checkout");
+    protected BaseCore baseCore;
+    protected Faker checkout_info = new Faker(Locale.ENGLISH);
+    //define locators
+    protected By firstname = By.id("first-name");
+    protected By lastname = By.id("last-name");
+    protected By zip_code = By.xpath("//input[@id='postal-code']");
+    protected By continue_btn = By.id("continue");
+    protected By overview = By.xpath("//span[@class='title']");
+    protected By finish_btn = By.id("finish");
+    protected By order_header = By.xpath("//h2[@class='complete-header']");
+
+    public CheckoutPage() {
+        this.baseCore = new BaseCore();
+    }
+
+    //fill the checkout information, using Faker library
+    public void fill_checkout_info() {
+        baseCore.typeRequiredValue(firstname, checkout_info.name().firstName());
+        baseCore.typeRequiredValue(lastname, checkout_info.name().lastName());
+        baseCore.typeRequiredValue(zip_code, checkout_info.address().zipCode());
+    }
+
+
 }
